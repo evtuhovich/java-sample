df3c3f0f69b7a361947152068122a91c043665a7
Vitaly Khabarov <vitkhab@gmail.com>
2016-03-22 14:00:06 +0300

    Added default attributes for java into project cookbook

--- cookbooks/java-sample/metadata.rb
+++ cookbooks/java-sample/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'vkhabarov@express42.com'
 license          'All rights reserved'
 description      'Installs/Configures java-sample'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.4'
+version          '0.1.5'
 
 depends          'tomcat'
 depends          'java'

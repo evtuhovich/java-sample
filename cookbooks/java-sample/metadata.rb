0b928a97eac5e3409307acb097d87803786775a5
Vitaly Khabarov <vitkhab@gmail.com>
2016-03-25 12:40:30 +0300

    Added java_version to cookbook attributes because tomcat 8.0.32 does not support java 1.6 installed by default

--- cookbooks/java-sample/metadata.rb
+++ cookbooks/java-sample/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'vkhabarov@express42.com'
 license          'All rights reserved'
 description      'Installs/Configures java-sample'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.8'
+version          '0.1.9'
 
 depends          'tomcat'
 depends          'java'
